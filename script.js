aaa37328d1449dde6ee59e03e14c1acb4df5d1ee
hex-paradus <paradushex@gmail.com>
2021-09-06 01:50:20 +0600

    updated usernames

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 
 // Data
 const account1 = {
-  owner: 'Arnab Pahlavi',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: 'Code Wizards',
+  movements: [20000, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Safwan Saheer',
+  owner: 'Yaseen Nur',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
